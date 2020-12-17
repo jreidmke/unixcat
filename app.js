d2ddc6c161db98d23119e85b9597e97f774b27cd
James Reid <jreidmke@gmail.com>
2020-12-17 12:38:39 -0600

    yeh

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
 const argv = process.argv;
+const fs = require('fs');
+const axios = require('axios');
 
-for (let i = 0; i < argv.length; i += 1) {
-  console.log(i, argv[i]);
+// --out output-filename.txt readfile-or-url
+
+function cat(path, newFile) {
+    console.log(path);
+    if(argv[2] === '--out') {
+        fs.writeFile(newFile, path, 'utf-8', (err, data) => {
+            if(err) throw err;
+            console.log("FILE WRITTEN");
+        })
+    }
+    fs.readFile(path, 'utf-8', (err, data) => {
+        if(err) throw err;
+        console.log(data);
+    });
+}
+
+async function webCat(url) {
+    try {
+        const resp = await axios.get(url);
+        fs.readFile(resp.data, 'utf-8', (err, data) => {
+            if(err) throw err;
+            console.log(data)
+        })
+    } catch (error) {
+        console.log(error); 
+    }
+}
+
+if(argv[2] === '--out') {
+    cat(argv[4], argv[3]);
+} else {
+    cat(path = argv[2])
 }
